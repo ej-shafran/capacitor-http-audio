116a707d1dad79dd532cb94bb062f8249d028939
ej-shafran <ejshafran@hilma.tech>
2024-02-28 13:44:56 +0200

    Add working example for web

---
+++ main.js
@@ -0,0 +1,16 @@
+const AUDIO_URL =
+  "https://archive.org/download/Old_Radio_Adverts_01/OldRadio_Adv--1957_Chevrolet.mp3";
+
+async function main() {
+  document.querySelector("#output").textContent = "Loading...";
+
+  const res = await fetch(AUDIO_URL);
+  const arrayBuffer = await res.arrayBuffer();
+  const ctx = new AudioContext();
+  const data = (await ctx.decodeAudioData(arrayBuffer)).getChannelData(0);
+
+  document.querySelector("#output").textContent =
+    `Done! Got ${data.byteLength} bytes`;
+}
+
+main();
